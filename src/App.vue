1b1e76c268293958b224933db001316e0f18d56d
Jacob <975409698@qq.com>
2023-06-06 16:08:54 +0800

    删除功能完善

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import Edit from './components/Edit.vue'
 import axios from 'axios'
 import { onMounted,ref } from 'vue';
-
+import {ElMessage,ElMessageBox} from 'element-plus';
 // TODO: 列表渲染
 //1.声明响应式list
 const list=ref([])
@@ -10,20 +10,63 @@ const list=ref([])
 const getList=async()=>{
   //接口调用
   const res= await axios.get("http://localhost:8080/user")
-  console.log(res)
   //3.后端数据复制给list
   list.value=res.data
 }
 
 onMounted(()=>
   getList()
+
 )
 
 
-//4.绑定到table组件
+
 
 // TODO: 删除功能
+//1.获取当前行id
 
+
+const deleteItem=async(id)=>{
+  console.log(id)
+  //2.通过id调用删除接口
+    ElMessageBox.confirm(
+      '是否确定删除',
+      '警告',
+      {
+        confirmButtonText:"确定",
+        cancelButtonText:'取消'
+      }
+    ).then(async ()=>{
+      const res=await axios.delete(`http://localhost:8080/user/${id}`)
+      if(res.data.code!=1){
+        ElMessage({
+          message:`操作失败:${res.data.msg}`,
+          type:'error'
+        })
+      }else{
+        console.log(res.data)
+        ElMessage({
+          message:res.data.data,
+          type:'success'
+        })
+        getList()
+      }
+       
+      }
+    ).catch(()=>{
+        ElMessage({
+          message:'取消操作',
+          type:'info'
+        })
+    }
+    )
+   }
+  
+ 
+
+
+
+//3.更新最新的列表
 
 // TODO: 编辑功能
 
@@ -31,14 +74,15 @@ onMounted(()=>
 
 <template>
   <div class="app">
+    <!-- //4.绑定到table组件 -->
     <el-table :data="list">
       <el-table-column label="ID" prop="id"></el-table-column>
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="birthPlace"></el-table-column>
       <el-table-column label="操作" width="150">
-        <template #default>
+        <template #default ={row}>
           <el-button type="primary" link>编辑</el-button>
-          <el-button type="danger" link>删除</el-button>
+          <el-button type="danger" link @click="deleteItem(row.id)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
